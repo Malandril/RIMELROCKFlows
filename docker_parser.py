566db1f99e5ec9d5ba610e1927c14917e5965018
Thomas Canava <canava.thomas@gmail.com>
2019-01-29 10:41:42 +0100

    fixed docker parser

--- docker_parser.py
+++ docker_parser.py
@@ -3,12 +3,12 @@ import json
 import pathlib
 import re
 
-DOCKERFILE_PATTERN = "Dockerfile"
+DOCKERFILE_PATTERN = "Dockerfile$"
 BUILD_SCRIPT = "build.sh"
 
-DOCKER_BUILD_PATTERN = r"docker build .*-t (\S+).* (\S+)$"
+DOCKER_BUILD_PATTERN = r"docker build .*-t ([\w\-.\/]+).* (\S+)$"
 DOCKER_BUILD_F_PARAMETER = "-f (.*) .*"
-DOCKER_FROM_PATTERN = "from (.*)"
+DOCKER_FROM_PATTERN = "from ([\w\-.\/]+).*"
 
 
 def find_files(path, pattern=DOCKERFILE_PATTERN):
@@ -54,48 +54,60 @@ def main():
             dockerfile_paths += find_files(project, DOCKERFILE_PATTERN)
             build_scripts += find_files(project, BUILD_SCRIPT)
         for dockerfile in dockerfile_paths:
-            dockerfiles.append({"path": dockerfile, "from": parse_file(dockerfile, DOCKER_FROM_PATTERN, 1)[0]})
-        result = []
+            dockerfiles.append({"path": dockerfile, "from": parse_file(
+                dockerfile, DOCKER_FROM_PATTERN, 1)[0]})
         nodes = []
         links = []
-        for script in build_scripts:
-            res = parse_file(script, DOCKER_BUILD_PATTERN, 1, 2, 0)
-            if res:
-                image_name, relative_path = res[0], res[1]
-                match = re.search(DOCKER_BUILD_F_PARAMETER, res[2])
-                if match:
-                    relative_path = str(pathlib.Path(match.group(1)).parent)
-                joinpath = script.parent.joinpath(relative_path)
-                for dockerfile in dockerfiles:
-                    if dockerfile["path"].match(str(joinpath) + "/*"):
-                        dockerfile["name"] = image_name
-                        for project in projects:
-                            print(project.name, list(joinpath.parents))
-                            if project == joinpath or project in joinpath.parents:
-                                dockerfile["name"] = str(project.name)
-                                break
-                        result.append((dockerfile["name"], dockerfile["from"]))
 
-                        node = {"id": dockerfile["name"]}
-                        if node not in nodes:
-                            nodes.append(node)
-                            start = len(nodes) - 1
-                        else:
-                            start = nodes.index(node)
-                        node = {"id": dockerfile["from"]}
-                        if node not in nodes:
-                            nodes.append(node)
-                            end = len(nodes) - 1
-                        else:
-                            end = nodes.index(node)
-                        links.append({"source": start, "target": end})
+        link_build_scripts_and_dockerfiles(build_scripts, dockerfiles, projects)
+        res = filter(lambda x: "name" not in x, dockerfiles)
+        print(len(dockerfiles), len(list(res)))
 
-                        break
+        for dockerfile in dockerfiles:
+            for df in dockerfiles:
+                if "name" in dockerfile and df["from"] == dockerfile["name"]:
+                    df["from"] = dockerfile["project_name"]
 
-        with open("res.json", "w") as f:
-            json.dump(result, f)
+            if "name" in dockerfile:
+                node = {"id": dockerfile["project_name"] if "project_name" in dockerfile else dockerfile["name"]}
+                if node not in nodes:
+                    nodes.append(node)
+                    start = len(nodes) - 1
+                else:
+                    start = nodes.index(node)
+                node = {"id": dockerfile["from"]}
+                if node not in nodes:
+                    nodes.append(node)
+                    end = len(nodes) - 1
+                else:
+                    end = nodes.index(node)
+                links.append({"source": start, "target": end})
+
         with open("D3_visualisation/graph_rockflows.json", "w") as f:
             json.dump({"nodes": nodes, "links": links}, f)
+
+
+def link_build_scripts_and_dockerfiles(build_scripts, dockerfiles, projects):
+    i = 0
+    for script in build_scripts:
+        res = parse_file(script, DOCKER_BUILD_PATTERN, 1, 2, 0)
+        if res:
+            image_name, relative_path = res[0], res[1]
+            print(res)
+            match = re.search(DOCKER_BUILD_F_PARAMETER, res[2])
+            if match:
+                relative_path = str(pathlib.Path(match.group(1)).parent)
+            joinpath = script.parent.joinpath(relative_path)
+
+            for dockerfile in dockerfiles:
+
+                if dockerfile["path"].match(str(joinpath) + "/*"):
+                    dockerfile["name"] = image_name
+                    i += 1
+                    for project in projects:
+                        if project == joinpath or project in joinpath.parents:
+                            dockerfile["project_name"] = str(project.name)
+                            break
 
 
 if __name__ == '__main__':
